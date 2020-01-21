d444c309c996b362aa053413d181f393d7dda5f4
Priscila Oliveira <priscila.oliveira50@fatec.sp.gov.br>
2020-01-20 23:52:20 -0300

    Config URL darksky

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/adb7b35147130a9e9494246f8f756ab9/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/adb7b35147130a9e9494246f8f756ab9/-23.2245916,-45.923067?lang=pt";
 
 request({ url: url, json: true }, (error, response) => {
-  const data = response.body.currently;
+  const base = response.body;
   // console.log(response.body.currently)
   console.log(
-    "A temperatura atual é de: " +
-      data.temperature +
-      ", com probabilidade de chuva de: " +
-      data.precipProbability +
-      "%"
+    base.daily.data[0].summary +
+      "A temperatura atual é de: " +
+      base.currently.temperature +
+      " graus celcius." +
+      " Com probabilidade de : " +
+      base.currently.precipProbability +
+      "% de preceptação"
   );
 });
