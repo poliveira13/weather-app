3ba971f6db8045fef7850bc658ded955da33fe1a
Priscila Oliveira <priscila.oliveira50@fatec.sp.gov.br>
2020-01-20 22:54:11 -0300

    Primeiro exercício
    Passar as infos pedidas pelo professor no console para imprimir no terminal

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const request = require("request");
 const url =
   "https://api.darksky.net/forecast/adb7b35147130a9e9494246f8f756ab9/37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.currently;
+  // console.log(response.body.currently)
+  console.log(
+    "A temperatura atual é de: " +
+      data.temperature +
+      ", com probabilidade de chuva de: " +
+      data.precipProbability +
+      "%"
+  );
 });
